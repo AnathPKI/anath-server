40fa7b06b719f9769b0ad9ad8d39d8bfe320f3a6
Rafael Ostertag <rafi@guengel.ch>
2018-04-01 08:27:12 +0200

    Annotated with @ResponseStatus.

--- src/main/java/ch/zhaw/ba/anath/pki/exceptions/CertificateAuthorityInitializationException.java
+++ src/main/java/ch/zhaw/ba/anath/pki/exceptions/CertificateAuthorityInitializationException.java
@@ -30,10 +30,13 @@
 package ch.zhaw.ba.anath.pki.exceptions;
 
 import ch.zhaw.ba.anath.AnathException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * @author Rafael Ostertag
  */
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class CertificateAuthorityInitializationException extends AnathException {
     public CertificateAuthorityInitializationException(String message) {
         super(message);
