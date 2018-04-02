3f3f93deb7d4060b4b345d5566a7c7e6d2b8235c
Rafael Ostertag <rafi@guengel.ch>
2018-04-02 18:17:51 +0200

    Added CrlMaintenanceTask.

--- src/main/java/ch/zhaw/ba/anath/Application.java
+++ src/main/java/ch/zhaw/ba/anath/Application.java
@@ -31,11 +31,13 @@ package ch.zhaw.ba.anath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author Rafael Ostertag
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
