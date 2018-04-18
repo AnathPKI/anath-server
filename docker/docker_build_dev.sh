bc2feeb59f324ad2236e205d30eea859dbd1da43
Rafael Ostertag <rafi@guengel.ch>
2018-04-18 22:57:35 +0200

    Trying to figure out why deploy fails.

--- docker/docker_build_dev.sh
+++ docker/docker_build_dev.sh
@@ -2,5 +2,8 @@
 # Build development release
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+set -x
+
 docker build .. -t anath/pki:test
 docker push anath/pki:test
